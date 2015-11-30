cc0278d5c5fc6205ca656808576e2569fec95e62
Oscar Rodriguez <Oscar.Rodriguez40@t-mobile.com>
2015-11-30 01:21:27 -0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kikisApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kikisApp
+ */
+angular.module('kikisApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kikisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kikisApp
+ */
+angular.module('kikisApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
